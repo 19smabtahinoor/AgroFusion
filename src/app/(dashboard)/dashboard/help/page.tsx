2cb8ff67dfaaf93e3b405bc2bc396211d103461d
19smabtahinoor <abtahi.educxy@gmail.com>
2024-10-02 04:49:58 +0600

    Help center name changed

--- src/app/(dashboard)/dashboard/help/page.tsx
+++ src/app/(dashboard)/dashboard/help/page.tsx
@@ -1,6 +1,11 @@
+'use client';
 import HelpChat from '@/components/help/help_chat';
+import { useSession } from 'next-auth/react';
 
 export default function Page() {
+  const { data } = useSession();
+  const { user } = data || {};
+
   return (
     <>
       <main className="lg:px-16 lg:pt-16">
@@ -8,7 +13,7 @@ export default function Page() {
         <div className="flex flex-col items-center justify-center space-y-1">
           <p className="text-lg lg:text-xl text-center">
             Hello,{' '}
-            <span className="text-primary font-semibold">Abtahi Noor</span>
+            <span className="text-primary font-semibold">{user?.name}</span>
           </p>
           <h1 className="font-semibold text-2xl lg:text-5xl text-slate-400 opacity-70">
             How can I help you today ?
