60ed61f09cc2e27386c5094613a25e81a2bfbfcf
19smabtahinoor <abtahi.educxy@gmail.com>
2024-10-03 20:32:51 +0600

    Top bar location changed

--- src/components/shell/TopBarLocation.tsx
+++ src/components/shell/TopBarLocation.tsx
@@ -10,17 +10,20 @@ const TopBarLocation = () => {
       .then(response => response.json())
       .then(result => {
         setLocation(result?.features[0]?.properties);
+        
       }
       )
       .catch(error => console.log('error', error));
   }, []);
 
+  console.log(location)
 
   return (
     <div className='border border-slate-200 px-2 py-3 rounded-full flex flex-row items-center space-x-2'>
 
       <FaLocationDot className='text-primary text-lg' />
-      <h1 className='text-xs'>{location?.address_line1}</h1>
+      <h1 className='text-xs'>Mohammadpur,Dhaka</h1>
+      {/* <h1 className='text-xs'>{location?.address_line1}</h1> */}
 
     </div>
   );
