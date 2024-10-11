86d8d0a11daef11e9721acb82604b5ebe1f2e5a6
19smabtahinoor <abtahi.educxy@gmail.com>
2024-10-11 11:30:08 +0600

    hero section of landing page updated

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -9,7 +9,8 @@ export default function About() {
                     <Image src={icons.agriculturalRobot} className="w-full lg:w-[200px] lg:mr-5" alt="robot" />
                     <div>
                         <h1 className="text-2xl lg:text-3xl font-bold text-primary mb-2 text-center lg:text-left">About Us</h1>
-                        <p className="text-sm w-full text-justify lg:w-5/6">We developed <span className="text-primary font-bold">AgroFusion</span> to empower farmers with real-time data and actionable insights, helping them navigate unpredictable weather, manage water resources, and protect their crops. By integrating NASA’s datasets with advanced sensor technology, our app provides a comprehensive overview of field conditions, enabling farmers to make informed decisions and ensure a sustainable future for their farms</p>
+                        <p className="text-sm w-full text-justify lg:w-5/6">Our team, &apos;Team AgroFusion,&apos; created &apos;AgroFusion,&apos; a groundbreaking blend of cutting-edge software and hardware, designed to be the ultimate all-in-one agricultural solution. This powerful web app equips farmers with real-time field data, expert farming advice driven by detection systems and NASA datasets, predictive insights for the future, critical alerts, and AI-powered advisory and support. By delivering precise and actionable information, AgroFusion tackles key challenges like water management, disaster prevention, crop protection, and yield optimization. The goal is clear: to revolutionize farming, slash crop losses, boost production, and protect livelihoods, ultimately curbing agricultural economic crises and reducing global food insecurity.
+                        </p>
                     </div>
                 </div>
                 <div className="flex flex-col lg:flex-row lg:absolute lg:top-[95%] items-center lg:left-[15%] lg:-z-10">
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 export default function Hero() {
     return (
         <header>
-            <div className="w-5/6 mx-auto flex flex-col md:flex-row items-center mt-[50px] mb-[120px]">
+            <div className="w-5/6 mx-auto flex flex-col space-y-8 md:flex-row items-center mt-[50px] mb-[120px]">
 
                 <div className="relative">
-                    <div className="text-6xl font-bold flex mb-[8%]">
-                        <h1 className="text-primary">Agro</h1>
-                        <span>Fusion</span>
+                    <div className="flex flex-col items-center lg:items-start space-y-4">
+                        <div className="text-3xl lg:text-5xl font-bold">
+                            <h1 className="text-black text-center lg:text-left leading-[32px] lg:leading-[55px]"><span className="text-primary">AgroFusion:</span> Sustainable Farming  Powered by <br /> Technology
+                            </h1>
+                        </div>
+                        <p className="text-slate-500 w-2/3 text-center lg:text-left text-sm">Empowering farmers with real-time insights to protect crops, manage resources, and thrive in any condition</p>
                     </div>
-                    <p>Empowering farmers with real-time insights to protect crops, manage resources, and thrive in any condition</p>
                     <div className="hidden lg:block w-[600px] h-[600px] bg-primary absolute filter opacity-10 -top-[400px] rotate-45 -left-[200px] blur-3xl"></div>
                 </div>
 
