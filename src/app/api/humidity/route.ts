be585287cf7af6ecaf6c50070e3ce0438c348055
19smabtahinoor <abtahi.educxy@gmail.com>
2024-11-10 02:57:03 +0600

    humidity api updated

--- src/app/api/humidity/route.ts
+++ src/app/api/humidity/route.ts
@@ -3,7 +3,6 @@ import sendResponse from "@/lib/functions/sendResponse";
 import HumidityModel from "@/models/Humidity";
 import { Humidity } from "@/types/types";
 import { NextRequest } from "next/server";
-import { subDays } from 'date-fns';
 
 export async function POST(request: NextRequest) {
     await dbConnect();
@@ -30,40 +29,21 @@ export async function POST(request: NextRequest) {
 // }
 
 
-// export async function GET() {
-//     await dbConnect();
-//     try {
-//         const startOfDay = new Date();
-//         startOfDay.setHours(0, 0, 0, 0);
-
-//         const endOfDay = new Date();
-//         endOfDay.setHours(23, 59, 59, 999);
-
-//         const humidities = await HumidityModel.find({}) as Humidity[];
-
-//         return sendResponse(true, `Today's water levels sent successfully, count: ${humidities.length}`, 200, humidities);
-
-//     } catch (error) {
-//         console.log("🚀 ~ GET ~ error: /api/water level - error fetching today's water levels", error);
-//         return sendResponse(false, 'Error fetching water levels', 400, error);
-//     }
-// }
-
 export async function GET() {
     await dbConnect();
     try {
-        // Get the date 10 days ago from today
-        const tenDaysAgo = subDays(new Date(), 10);
+        const startOfDay = new Date();
+        startOfDay.setHours(0, 0, 0, 0);
 
-        // Find humidity data from the last 10 days
-        const humidities = await HumidityModel.find({
-            timestamp: { $gte: tenDaysAgo }
-        }) as Humidity[];
+        const endOfDay = new Date();
+        endOfDay.setHours(23, 59, 59, 999);
 
-        return sendResponse(true, `Humidities from the last 10 days sent successfully, count: ${humidities.length}`, 200, humidities);
+        const humidities = await HumidityModel.find({}) as Humidity[];
+
+        return sendResponse(true, `Today's water levels sent successfully, count: ${humidities.length}`, 200, humidities);
 
     } catch (error) {
-        console.log("🚀 ~ GET ~ error: Error fetching humidity data", error);
-        return sendResponse(false, 'Error fetching humidity data', 400, error);
+        console.log("🚀 ~ GET ~ error: /api/water level - error fetching today's water levels", error);
+        return sendResponse(false, 'Error fetching water levels', 400, error);
     }
 }
