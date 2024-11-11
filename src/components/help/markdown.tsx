791080ed5b70515122806ce41b51239b896b45f0
19smabtahinoor <abtahi.educxy@gmail.com>
2024-11-12 02:16:13 +0600

    little changes

--- src/components/help/markdown.tsx
+++ src/components/help/markdown.tsx
@@ -10,6 +10,7 @@ const md = markdownit({});
 const Markdown = ({ text }: Props) => {
   const htmlcontent = md.render(text);
   const sanitized = DOMPurify.sanitize(htmlcontent);
+
   return <div dangerouslySetInnerHTML={{ __html: sanitized }} className='text-wrap'></div>;
 };
 
